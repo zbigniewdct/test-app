301a1ebecaa9a9ed0b90fd5d37c110b3928de0e9
Zbigniew Kucharczyk <zbignew-directit@outlook.com>
2018-10-08 12:15:45 -0400

    My test app.

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -10,6 +10,7 @@ namespace TestApp
     {
         static void Main(string[] args) {
             Console.WriteLine("My Test App");
+            Console.WriteLine("Learning GitHub");
         }
     }
 }
